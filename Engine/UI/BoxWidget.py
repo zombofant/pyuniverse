zombofant/pyuniverse

commit d805e4aec5caf67fd32953f6a898fd7b9467d1ce

Refactored widgets. This also fixes regression introduced by fe0f9f6af1f4220f0972b1d3ddef679464d8c711

Engine/UI/BoxWidget.py
@@ -1,4 +1,4 @@
-# File name: __init__.py
+# File name: BoxWidget.py
 # This file is part of: pyuni
 #
 # LICENSE
@@ -22,13 +22,7 @@
 # For feedback and questions about pyuni please e-mail one of the
 # authors named in the AUTHORS file.
 ########################################################################
-from CSS.Rect import *
-from WidgetBase import *
-from BoxWidget import *
-try:
-    import pyglet
-    from ScreenWidget import *
-    from RootWidget import *
-    from SceneWidget import *
-except ImportError:
-    pass
+from __future__ import unicode_literals, print_function, division
+from our_future import *
+
+

Engine/Application.py
@@ -61,8 +61,8 @@ class Application(RootWidget):
 
     def _constructWindowed(self, geometry):
         window, widget = self._newScreen((0, 0), geometry)
-        self.Rect.Width = window.width
-        self.Rect.Height = window.height
+        self.AbsoluteRect.Width = window.width
+        self.AbsoluteRect.Height = window.height
 
     def _constructFullscreen(self):
         """
@@ -88,13 +88,13 @@ class Application(RootWidget):
             totalWidth = max(totalWidth, x + w)
             totalHeight = max(totalHeight, y + h)
 
-        self.Rect.Width = totalWidth
-        self.Rect.Height = totalHeight
+        self.AbsoluteRect.Width = totalWidth
+        self.AbsoluteRect.Height = totalHeight
 
     def _newScreen(self, ui_logical, geometry, fullscreen=False, primary=True, screen=None):        
         window = self.makeWin(ui_logical, None if fullscreen else geometry, screen)
         widget = ScreenWidget(self, window)
-        widget.Rect.XYWH = (ui_logical[0], ui_logical[1], geometry[0], geometry[1])
+        widget.AbsoluteRect.XYWH = (ui_logical[0], ui_logical[1], geometry[0], geometry[1])
         t = (window, widget)
         self.windows.append(t)
         if primary:
@@ -103,8 +103,8 @@ class Application(RootWidget):
         return t
 
     def _buildFramebuffer(self):
-        w = makePOT(self.Rect.Width)
-        h = makePOT(self.Rect.Height)
+        w = makePOT(self.AbsoluteRect.Width)
+        h = makePOT(self.AbsoluteRect.Height)
         fbo = Framebuffer(w, h)
         uiTex = Texture2D(w, h, GL_RGBA8)
         fbo[GL_COLOR_ATTACHMENT0] = uiTex
@@ -130,8 +130,8 @@ class Application(RootWidget):
         vertexLists = []
         for window, widget in self.windows:
             vl = domain.create(4)
-            xmin, xmax = widget.Rect.Left / w, widget.Rect.Right / h
-            ymin, ymax = widget.Rect.Top / h, widget.Rect.Bottom / h
+            xmin, xmax = widget.AbsoluteRect.Left / w, widget.AbsoluteRect.Right / h
+            ymin, ymax = widget.AbsoluteRect.Top / h, widget.AbsoluteRect.Bottom / h
             texCoords = [
                 xmin, ymax,
                 xmax, ymax,
@@ -156,7 +156,7 @@ class Application(RootWidget):
             raise ValueError("Scene widgets must derive from SceneWidget")
         screen = self._getWidgetScreen(widget)
         assert screen is not None
-        if screen.Rect & widget.Rect != widget.Rect:
+        if screen.AbsoluteRect & widget.AbsoluteRect != widget.AbsoluteRect:
             raise ValueError("Scene widget leaves screen boundaries")
         window = screen.Window
         assert window is not None
@@ -254,7 +254,7 @@ class Application(RootWidget):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         
         for sceneWidget in window._sceneWidgets:
-            x, y, w, h = sceneWidget.Rect.XYWH
+            x, y, w, h = sceneWidget.AbsoluteRect.XYWH
             xlog, ylog = window.UILogicalCoords
             x -= xlog
             y -= ylog

Engine/GL/StateManagement.py
@@ -90,12 +90,12 @@ class StateObjectGroup(Group):
     """
     
     def __init__(self, *args, **kwargs):
-        self.parent = None
+        parent = None
         # 2to3: this can be made better in python3 using keyword-only
         # arguments
         if "parent" in kwargs:
             parent = kwargs.pop("parent")
-        super(Group, self).__init__(parent=parent, **kwargs)
+        super(StateObjectGroup, self).__init__(parent=parent, **kwargs)
         setCalls = []
         unsetCalls = []
         for stateObj in args:

Engine/UI/RootWidget.py
@@ -28,7 +28,7 @@ from our_future import *
 __all__ = ["RootWidget"]
 
 from Engine.pygletHeadless import mouse
-from Widget import AbstractWidget, WidgetContainer
+from WidgetBase import AbstractWidget, WidgetContainer
 from Flags import *
 
 """

Engine/UI/SceneWidget.py
@@ -1,4 +1,4 @@
-# File name: Screen.py
+# File name: Scene.py
 # This file is part of: pyuni
 #
 # LICENSE
@@ -25,25 +25,31 @@
 from __future__ import unicode_literals, print_function, division
 from our_future import *
 
-__all__ = ["ScreenWidget"]
+__all__ = ["SceneWidget"]
 
-from Widget import ParentWidget
+from WidgetBase import Widget
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
-class ScreenWidget(ParentWidget):
-    """
-    Represents an operating system screen or window.
+class SceneWidget(Widget):
+    def __init__(self, parent, **kwargs):
+        # FIXME: pass scene graph and camera to use ;)
+        super(SceneWidget, self).__init__(parent, **kwargs)
+        self.FOV = 60.0
+        self.ZNear = 1.0
+        self.ZFar = 100.0
 
-    This is used by the RootWidget *Application* to manage windows.
-    """
-    
-    def __init__(self, parent, window, **kwargs):
-        super(ScreenWidget, self).__init__(parent, **kwargs)
-        self._window = window
+    def _setupProjection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(self.FOV, self.AbsoluteRect.Width / self.AbsoluteRect.Height, self.ZNear, self.ZFar)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
-    def align(self):
-        for child in self:
-            child.AbsoluteRect.assign(self.Rect)
+    def _resetProjection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW)
 
-    @property
-    def Window(self):
-        return self._window
+    def renderScene(self):
+        pass

Engine/UI/ScreenWidget.py
@@ -1,4 +1,4 @@
-# File name: Scene.py
+# File name: Screen.py
 # This file is part of: pyuni
 #
 # LICENSE
@@ -25,31 +25,25 @@
 from __future__ import unicode_literals, print_function, division
 from our_future import *
 
-__all__ = ["SceneWidget"]
+__all__ = ["ScreenWidget"]
 
-from Widget import Widget
-from OpenGL.GL import *
-from OpenGL.GLU import *
+from WidgetBase import ParentWidget
 
-class SceneWidget(Widget):
-    def __init__(self, parent, **kwargs):
-        # FIXME: pass scene graph and camera to use ;)
-        super(SceneWidget, self).__init__(parent, **kwargs)
-        self.FOV = 60.0
-        self.ZNear = 1.0
-        self.ZFar = 100.0
+class ScreenWidget(ParentWidget):
+    """
+    Represents an operating system screen or window.
 
-    def _setupProjection(self):
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(self.FOV, self.AbsoluteRect.Width / self.AbsoluteRect.Height, self.ZNear, self.ZFar)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
+    This is used by the RootWidget *Application* to manage windows.
+    """
+    
+    def __init__(self, parent, window, **kwargs):
+        super(ScreenWidget, self).__init__(parent, **kwargs)
+        self._window = window
 
-    def _resetProjection(self):
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glMatrixMode(GL_MODELVIEW)
+    def align(self):
+        for child in self:
+            child.AbsoluteRect.assign(self.AbsoluteRect)
 
-    def renderScene(self):
-        pass
+    @property
+    def Window(self):
+        return self._window

commit 17816b820f9269e92bc12514e7d97913d40f5e42

Fixed more regressions introduced by d805e4aec5caf67fd32953f6a898fd7b9467d1ce

Engine/UI/BoxWidget.py
@@ -25,4 +25,5 @@
 from __future__ import unicode_literals, print_function, division
 from our_future import *
 
+# __all__ = ["VBox", "HBox", "Grid"]
 

Engine/UI/CSS/Minilanguage.py
@@ -49,6 +49,19 @@ class StylesheetNamespace(object):
 
     _tokenBlacklist = ["evaluateCall", "get"]
 
+    __singleton = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__singleton is None:
+            return super(type(StylesheetNamespace), cls).__new__(cls, *args, **kwargs)
+        else:
+            return cls.__singleton
+
+    def __init__(self):
+        if not hasattr(self, "_initialized"):
+            super(StylesheetNamespace, self).__init__()
+            self._initialized = True
+
     def evaluateCall(self, call, *args):
         call = self.get(call)
         return call(*args)
@@ -59,5 +72,34 @@ class StylesheetNamespace(object):
             raise ValueError("Token {0!r} not defined in css".format(token))
         return getattr(self, token)
 
-elementNames = {}
+class ElementNames(dict):
+    __singleton = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls.__singleton is None:
+            cls.__singleton = dict.__new__(cls, *args, **kwargs)
+            return cls.__singleton
+        else:
+            return cls.__singleton
+
+    def __init__(self):
+        if not hasattr(self, "_initialized"):
+            super(ElementNames, self).__init__()
+            self._initialized = True
+
+    def __getitem__(self, key):
+        return super(ElementNames, self).__getitem__(key.lower())
+
+    def __setitem__(self, key, value):
+        return super(ElementNames, self).__setitem__(key.lower(), value)
+
+    def __delitem__(self, key):
+        return super(ElementNames, self).__delitem__(key.lower())
+
+    def update(self, otherdict):
+        for key, value in otherdict.iteritems():
+            self[key] = value
+
+elementNames = ElementNames()
 namespace = StylesheetNamespace()
+    

Engine/UI/Theme.py
@@ -28,7 +28,7 @@ from our_future import *
 import itertools
 
 from Style import Style
-from Widget import WidgetContainer
+from WidgetBase import WidgetContainer
 
 class SelectorTuple(tuple):
     def __eq__(self, other):

Engine/UI/WidgetBase.py
@@ -65,7 +65,7 @@ class AbstractWidget(object):
         self._invalidateAlignment()
 
     def _invalidateGeometry(self):
-        self._invalidateGeometry = True
+        self._invalidatedGeometry = True
         self._geometry = None
 
     def _relMetricsChanged(self):

Engine/UI/test_Theme.py
@@ -29,8 +29,8 @@ import unittest
 
 from Style import Style
 from Theme import Theme
-from Widget import ParentWidget
-from Root import RootWidget
+from WidgetBase import ParentWidget
+from RootWidget import RootWidget
 from CSS.Parser import Parser
 from CSS.Fill import Transparent, Colour
 from CSS.Box import Padding, Margin

Engine/UI/test_Widget.py
@@ -22,8 +22,8 @@
 # For feedback and questions about pyuni please e-mail one of the
 # authors named in the AUTHORS file.
 ########################################################################
-from Widget import Widget, ParentWidget
-from Root import RootWidget
+from WidgetBase import Widget, ParentWidget
+from RootWidget import RootWidget
 import unittest
 
 class WidgetInit(unittest.TestCase):