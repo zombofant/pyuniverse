c724a8abab5a5295acaaaa6f4c00297869e55d02
Jonas Wielicki <j.wielicki@sotecware.net>
2012-03-07 16:25:00 +0100

    Refactored Synchronization primitives

---
+++ Logic/Synchronization/Properties.py
@@ -0,0 +1,60 @@
+# File name: Properties.py
+# This file is part of: pyuni
+#
+# LICENSE
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public license (the  "GPL License"), in which case
+# the provisions of GPL License are applicable instead of those above.
+#
+# FEEDBACK & QUESTIONS
+#
+# For feedback and questions about pyuni please e-mail one of the
+# authors named in the AUTHORS file.
+########################################################################
+from __future__ import unicode_literals, print_function, division
+from our_future import *
+
+from Synchronization import SyncClass
+
+class SynchronizedProperty(object):
+    def __init__(self, getter, setter=None, deleter=None):
+        super(SynchronizedProperty, self).__init__()
+        self._getter = getter
+        self._setter = None
+        self._deleter = None
+        self._sync = SyncClass()
+
+    def __get__(self, instance, owner):
+        return self._getter(instance) if instance is not None else self
+
+    def __set__(self, instance, value):
+        if self._setter is None:
+            raise AttributeError("Can't set attribute")
+        newValue = self._setter(instance, value)
+        for client in self._sync.iterSubscriptions(self, instance):
+            client.update((prop, instance), value)
+
+    def __delete__(self, instance):
+        # deletion should do a setting to a default value
+        if self._deleter is None:
+            raise AttributeError("Can't delete attribute")
+
+    def setter(self, setter):
+        self._setter = setter
+        return self
+
+    def deleter(self, deleter):
+        self._deleter = deleter
+        return self
+
---
+++ Logic/Synchronization/Service.py
@@ -0,0 +1,63 @@
+# File name: Service.py
+# This file is part of: pyuni
+#
+# LICENSE
+#
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 (the "License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the GNU General Public license (the  "GPL License"), in which case
+# the provisions of GPL License are applicable instead of those above.
+#
+# FEEDBACK & QUESTIONS
+#
+# For feedback and questions about pyuni please e-mail one of the
+# authors named in the AUTHORS file.
+########################################################################
+from __future__ import unicode_literals, print_function, division
+from our_future import *
+
+class Synchronization(object):
+    __singleton = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls is Synchronization:
+            raise TypeError("Cannot instanciate Synchronization directly. Set a synchronization class by setting Logic.Synchronization.SyncClass")
+        if cls.__singleton is not None:
+            return cls.__singleton
+        else:
+            return super(type(Synchronization), cls).__init__(*args, **kwargs)
+
+    def __init__(self, **kwargs):
+        if not hasattr(self, "_initialized"):
+            super(Synchronization, self).__init__(**kwargs)
+            self._initialized = True
+            self._clients = set()
+            self._properties = {}
+
+    def addClient(self, client):
+        self._clients.add(client)
+
+    def deleteClient(self, client):
+        if not client in self._clients:
+            return
+        for prop, instance in client:
+            self.removeSubscription(prop, instance, client)
+        self._clients.remove(client)
+
+    def addSubscription(self, property, instance, client):
+        self._properties[property].setdefault(instance, set()).add(client)
+
+    def removeSubscription(self, property, instance, client):
+        self._properties[property].get(instance, set()).remove(client)
+
+    def iterSubscriptions(self, property, instance):
+        return iter(self._properties[property].get(instance, ()))
