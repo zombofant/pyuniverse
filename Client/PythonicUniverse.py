b47a7a987b70aa3346385ba3bdafbf1ddb3ec278
Sebastian Riese <sebastian.riese.mail@web.de>
2012-05-13 19:10:04 +0200

    Added fullscreen capabilities to the X11Window

--- Client/PythonicUniverse.py
+++ Client/PythonicUniverse.py
@@ -169,11 +169,11 @@ class PythonicUniverse(Application):
         potW, potH = makePOT(w), makePOT(h)
 
         self.cairoTexCoords = (w / potW, h / potH)
-        
+
         self.cairoTex = Texture2D(
             potW, potH, format=GL_RGBA,
             data=(GL_RGBA, GL_UNSIGNED_BYTE, None))
-        
+
         self.cairoSurf = cairo.ImageSurface(
             cairo.FORMAT_ARGB32,
             w,
@@ -181,9 +181,9 @@ class PythonicUniverse(Application):
         )
         self._cairoContext = cairo.Context(self.cairoSurf)
         self._pangoContext = Pango.PangoCairoContext(self._cairoContext)
-            
+
         self.updateRenderingContext()
-        
+
         # sys.exit(1)
 
     def clearCairoSurface(self):
@@ -197,6 +197,13 @@ class PythonicUniverse(Application):
         if symbol == key.Escape:
             print("bye!")
             self._eventLoop.terminate()
+        elif symbol == key.f:
+            if self.fullscreen:
+                self._window.setWindowed(0, 800, 600)
+                self.fullscreen = False
+            else:
+                self._window.setFullscreen(0, 0, 0, 0)
+                self.fullscreen = True
 
     def cairoTesting(self):
         r = Rect()
@@ -263,8 +270,8 @@ class PythonicUniverse(Application):
                 cr.line_to(*x1y1)
                 cr.line_to(*x0y1)
                 cr.close_path()
-                
-            
+
+
         cr.set_line_width(1)
         cr.set_source_rgba(0.0, 0.5, 0.0, 1.0)
         cr.fill_preserve()
